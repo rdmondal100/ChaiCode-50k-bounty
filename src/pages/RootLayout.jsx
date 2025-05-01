b29a70739d7dd01d59788676469960668f3c1929
Riday <rdmondal100@gmail.com>
2025-05-01 10:34:12 +0600

    fix: blend the glow effect on the rootlayout

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -10,18 +10,21 @@ const RootLayout = () => {
 
 			<div className=' overflow-hidden relative'>
 				{/* Glows */}
-				<motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 0.07 }}
-					transition={{ duration: 2 }}
-					className='absolute top-[-100px] left-[-100px] w-[400px] h-[400px] bg-primary rounded-full blur-[120px] z-10'
-				/>
-				<motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 0.07 }}
-					transition={{ duration: 2, delay: 0.3 }}
-					className='absolute top-[-100px] right-[-100px] w-[400px] h-[400px] bg-primary rounded-full blur-[100px] z-10'
-				/>
+        <motion.div
+  initial={{ opacity: 0 }}
+  animate={{ opacity: 1 }}
+  transition={{ duration: 2 }}
+  className='absolute -top-32 -left-32 w-[400px] h-[400px] bg-primary/20 dark:bg-primary/15 rounded-full blur-[160px] z-10 pointer-events-none dark:mix-blend-screen'
+/>
+
+<motion.div
+  initial={{ opacity: 0 }}
+  animate={{ opacity: 1 }}
+  transition={{ duration: 2, delay: 0.3 }}
+  className='absolute -top-32 -right-32 w-[400px] h-[400px] bg-primary/20 dark:bg-primary/15 rounded-full blur-[160px] z-10 pointer-events-none dark:mix-blend-screen'
+/>
+
+
 				<main className='flex-1 relative overflow-hidden '>
 					<Outlet />
 				</main>
