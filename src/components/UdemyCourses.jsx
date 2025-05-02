8a5b8d7250da2cf05f297fdd10bed723e8a1a9a5
Riday <rdmondal100@gmail.com>
2025-05-02 10:58:42 +0600

    update: move the courses data to data folder

--- src/components/UdemyCourses.jsx
+++ src/components/UdemyCourses.jsx
@@ -19,6 +19,7 @@ import "swiper/css/navigation";
 import { ArrowBigLeft, ArrowBigRight } from "lucide-react";
 import RippleRingButton from "./RippleRingButton";
 import AnimatedWaveLine from "./AnimatedUnderLine";
+import { courses } from "@/data/udemyCoursesData";
 
 const StarIcon = () => (
 	<svg
@@ -125,46 +126,6 @@ const CourseCard = ({
 const UdemyCourses = () => {
 	const swiperRef = useRef(null);
 	const MotionRippleRingButton = motion.create(RippleRingButton);
-
-	const courses = [
-		{
-			title: "Complete web development course",
-			subtitle:
-				"Covers HTML, CSS, Tailwind, Node, React, MongoDB, Prisma, Deployment etc",
-			ytVideoCode:
-				"<iframe width='100%' height='100%' src='https://www.youtube.com/embed/KZ31wDjYleI' title='Most affordable web dev course launch' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>",
-			discountedPrice: "₹399",
-			regularPrice: "₹1199",
-			rating: 4.7,
-		},
-		{
-			title: "Full-Stack Mastery",
-			subtitle: "JavaScript, TypeScript, GraphQL, Postgres, Tailwind and more",
-			ytVideoCode:
-				"<iframe width='100%' height='100%' src='https://www.youtube.com/embed/KZ31wDjYleI' title='Full Stack Mastery course' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>",
-			discountedPrice: "₹599",
-			regularPrice: "₹2199",
-			rating: 4.2,
-		},
-		{
-			title: "Frontend Crash Course",
-			subtitle: "React, Tailwind, Framer Motion, Redux Toolkit",
-			ytVideoCode:
-				"<iframe width='100%' height='100%' src='https://www.youtube.com/embed/KZ31wDjYleI' title='Frontend crash course' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>",
-			discountedPrice: "₹999",
-			regularPrice: "₹4599",
-			rating: 3.9,
-		},
-		{
-			title: "API Development Bootcamp",
-			subtitle: "Node, Express, MongoDB, Prisma, REST, Postman",
-			ytVideoCode:
-				"<iframe width='100%' height='100%' src='https://www.youtube.com/embed/KZ31wDjYleI' title='API course' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>",
-			discountedPrice: "₹299",
-			regularPrice: "₹1499",
-			rating: 3.3,
-		},
-	];
 
 	return (
 		<section id='udemy' className='relative py-10 mb-10'>
