38375978e14605f789767f7b792a885488e8282f
Riday <rdmondal100@gmail.com>
2025-05-03 12:03:53 +0600

    update: move the topics to data folder

---
+++ src/data/cloudTopicsData.js
@@ -0,0 +1,61 @@
+export const topics = [
+    {
+      name: 'Python',
+      url: 'https://www.youtube.com/watch?v=Ca5DLSDfPec&list=PLu71SKxNbfoBsMugTFALhdLlZ5VOqCg2s',
+    },
+    {
+      name: 'Docker',
+      url: 'https://www.youtube.com/watch?v=rr9cI4u1_88&pp=ygUNZG9ja2VyIGhpdGVzaA%3D%3D',
+    },
+    {
+      name: 'React Native',
+      url: 'https://www.youtube.com/watch?v=kGtEax1WQFg&list=PLRAV69dS1uWSjBBJ-egNNOd4mdblt1P4c',
+    },
+    {
+      name: 'NextJS',
+      url: 'https://www.youtube.com/watch?v=OgS1ZWZItno&list=PLu71SKxNbfoBAaWGtn9GA2PTw0HO0tXzq',
+    },
+    {
+      name: 'NodeJS',
+      url: 'https://www.youtube.com/watch?v=EH3vGeqeIAo&list=PLu71SKxNbfoBGh_8p_NS-ZAh6v7HhYqHW',
+    },
+    {
+      name: 'TailwindCSS',
+      url: 'https://www.youtube.com/watch?v=eiRcOPiNoDs&t=48s&pp=ygUadGFpbHdpbmQgY3NzIGNoYWkgYXVyIGNvZGU%3D',
+    },
+    {
+      name: 'TypeScript',
+      url: 'https://www.youtube.com/watch?v=ZchBYjHFCC4&t=1552s&pp=ygUUdHlwZXNjcmlwdCBieSBoaXRlc2g%3D',
+    },
+  
+    {
+      name: 'React',
+      url: 'https://www.youtube.com/watch?v=vz1RlUyrc3w&list=PLu71SKxNbfoDqgPchmvIsL4hTnJIrtige',
+    },
+    {
+      name: 'JavaScript',
+      url: 'https://www.youtube.com/watch?v=Hr5iLG7sUa0&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37',
+    },
+    {
+      name: 'Pydantic',
+      url: 'https://youtu.be/rE-y-yMIeok?si=7cPkecFIEmqHsC1t',
+    },
+    
+    {
+      name: 'Jupyter Notebook',
+      url: 'https://www.youtube.com/watch?v=_mowKpIZpbU&list=PLu71SKxNbfoAvRjhCwrRx39NssrrHt95G',
+    },
+    {
+      name: 'Numpy',
+      url: 'https://www.youtube.com/watch?v=x7ULDYs4X84&list=PLu71SKxNbfoDzco7QLan_WqsRKndIycxH',
+    },
+    {
+      name: 'C++',
+      url: 'https://www.youtube.com/watch?v=gCkPJTSZ9mU&list=PLu71SKxNbfoCPfgKZS8UE0MDuwiKvL8zi&pp=0gcJCV8EOCosWNin',
+    },
+    {
+      name: 'Django',
+      url: 'https://www.youtube.com/watch?v=j6szNSzw4BU&list=PLu71SKxNbfoDOf-6vAcKmazT92uLnWAgy',
+    },
+  
+  ];
